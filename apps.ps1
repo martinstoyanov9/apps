acf1144c41e7a9d4f9c1dc7b4762936fcfafa28c
martinstoyanov9 <89925633+martinstoyanov9@users.noreply.github.com>
2021-09-01 21:50:32 +0300

    Update and rename Microsoft.PowerShell_profile.ps1 to apps.ps1

---
+++ apps.ps1
@@ -0,0 +1,10 @@
+function Do-ActualThing {
+    # do actual thing
+   
+    & C:\Users\martin.stoyanov\AppData\Local\Programs\Opera\launcher.exe
+    & C:\WINDOWS\system32\dsa.msc
+    & 'C:\Program Files (x86)\IBM\Lotus\Notes\notes.exe'
+    & 'C:\Users\martin.stoyanov\Desktop\Outlook 2016.lnk'
+
+    }
+
